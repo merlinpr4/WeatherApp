fe32e1c1f0d5e529566f0bb8529da272d5da2fff
Merlin <prasadme@tcd.ie>
2022-10-18 16:49:59 +0100

    folders for app

---
+++ Weather/server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+
+const https = require("https");
+
+const path=require("path")
+let publicPath= path.resolve(__dirname,"public")
+app.use(express.static(publicPath))
+
+app.get('/weather/:city', sendWeather)
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+function sendWeather(req,res) {
+    let city =  req.params.city;
+    console.log('getData');
+
+    let endpoint = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&units=metric&lang=en&appid=e84ba5f47d90887b1f24bf8990e92a28";
+
+
+    https.get(endpoint,(response) => {
+        response.on("data", (data) => res.send(data))
+    })
+
+   
+
+}
